0f053d396384dd8155efee9f971f56f16f279e3e
Sandy Abasman <abasifrekesandy@gmail.com>
2026-08-25 11:23:09 +0200

    fix: navbar not updating on user logout

--- app/Http/Controllers/Auth/Logout.php
+++ app/Http/Controllers/Auth/Logout.php
@@ -8,16 +8,14 @@ use Illuminate\Support\Facades\Auth;
 
 class Logout extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Request $request)
     {
-        
+        Auth::logout();
+
         // Invalidate session
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-           return redirect('/')->with('success', 'you are logged out!');
+        return redirect('/')->with('success', 'You have been logged out.');
     }
 }
