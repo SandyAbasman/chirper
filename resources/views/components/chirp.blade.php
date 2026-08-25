1355ed23b8dad18374c85db847a0f2a82eede665
Sandy Abasman <abasifrekesandy@gmail.com>
2026-08-25 10:52:23 +0200

    only authenticated users can update their post

--- resources/views/components/chirp.blade.php
+++ resources/views/components/chirp.blade.php
@@ -32,20 +32,22 @@
                         @endif
                     </div>
 
-                    <div class="flex gap-1">
-                        <a href="/chirps/{{ $chirp->id }}/edit" class="btn btn-ghost btn-xs">
-                            Edit
-                        </a>
+                    // only show edit and delete buttons if the authenticated user is the owner of the chirp
+                    @if (auth()->id() === $chirp->user_id)
+                        <div class="flex gap-1">
+                            <a href="/chirps/{{ $chirp->id }}/edit" class="btn btn-ghost btn-xs"> Edit </a>
 
 
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" onclick="return confirm('Are you sure you want to delete this chirp?')"
-                            class="btn btn-ghost btn-xs text-error">
-                            Delete
-                        </button>
-                        </form>
-                    </div>
+                            <form method="POST" action="/chirps/{{ $chirp->id }}"> @csrf
+                                @method('DELETE')
+                                <button type="submit"
+                                    onclick="return confirm('Are you sure you want to delete this chirp?')"
+                                    class="btn btn-ghost btn-xs text-error">
+                                    Delete
+                                </button>
+                            </form>
+                        </div>
+                    @endif
                 </div>
                 <p class="mt-1">{{ $chirp->message }}</p>
             </div>
